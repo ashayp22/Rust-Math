09cf906db59c4abbd73be190ea418c8607ee420e
jimit200 <jimit.gosar200@gmail.com>
2021-11-24 14:11:28 -0600

    Added sliders for rgb and branch angle
    Also benchmarked single-threaded performance. Not sure if multithreading can be easily implemented due to mutable references needed

--- src/apps/h_tree.rs
+++ src/apps/h_tree.rs
@@ -1,33 +1,56 @@
 use eframe::{egui, epi};
 use std::mem::swap;
 use egui::{containers::*, widgets::*, *};
-
+extern crate time;
+use time::PreciseTime;
 #[derive(PartialEq)]
 #[cfg_attr(feature = "serde", derive(serde::Deserialize, serde::Serialize))]
 #[cfg_attr(feature = "serde", serde(default))]
 pub struct HTree {
     paused: bool,
+    time: f64,
     zoom: f32,
+    start_line_width: f32,
+    depth: usize,
+    length_factor: f32,
+    luminance_factor: f32,
+    width_factor: f32,
+    line_count: usize,
     n: f32,
     last_n: f32,
-    shapes: Vec<Shape>
+    shapes: Vec<Shape>,
+    _branch_angle: f32,
+    r: u8,
+    g: u8,
+    b: u8
 }
 
 impl Default for HTree {
     fn default() -> Self {
         Self {
             paused: false,
+            time: 0.0,
             zoom: 0.25,
+            start_line_width: 2.5,
+            depth: 9,
+            length_factor: 0.8,
+            luminance_factor: 0.8,
+            width_factor: 0.9,
+            line_count: 0,
             n: 1.0,
             last_n: 1.0,
-            shapes:Vec::new()
+            shapes:Vec::new(),
+            _branch_angle: 0.26,
+            r:255,
+            g:0,
+            b:0
         }
     }
 }
 
 impl epi::App for HTree {
     fn name(&self) -> &str {
-        "H Tree"
+        "Htree"
     }
 
     fn update(&mut self, ctx: &egui::CtxRef, _frame: &mut epi::Frame<'_>) {
@@ -36,6 +59,14 @@ impl epi::App for HTree {
             .show(ctx, |ui| self.ui(ui));
     }
 }
+
+
+
+
+
+
+
+
 
 impl HTree {
     pub fn ui(&mut self, ui: &mut Ui) {
@@ -69,42 +100,56 @@ impl HTree {
     fn options_ui(&mut self, ui: &mut Ui) {
         // ui.checkbox(&mut self.paused, "Paused");
         ui.add(Slider::new(&mut self.n, 1.0..=3.0).text("N"));
+        ui.add(Slider::new(&mut self._branch_angle, 0.01..=1.0).text("Branch angle"));
+        ui.add(Slider::new(&mut self.r, 0..=255).text("r"));
+
+        ui.add(Slider::new(&mut self.g, 0..=255).text("g"));
+        ui.add(Slider::new(&mut self.b, 0..=255).text("b"));
+
+
+        
         // ui.add(Slider::new(&mut self.zoom, 0.0..=1.0).text("zoom"));
         egui::reset_button(ui, self);
     }
     
-    fn drawtree( &mut self, length: f32, x1: f32, y1: f32, angle: f32,painter: &Painter) {
+    fn drawtree( &mut self, length: f32, x1: f32, y1: f32, angle: f32,painter: &Painter, start:PreciseTime ) {
                    
             
         let _scaling_factor = 0.87;
-    let _branch_angle = 0.26; //0.26;
+   // let _branch_angle = 0.26; //0.26;
     let _min_branch_length = 20.0*(4.0-self.n);
-    let xr = x1 + ((angle - _branch_angle).cos() * length);
-    let yr = y1 - ((angle - _branch_angle).sin() * length);
-    let xl = x1 + ((angle + _branch_angle).cos() * length);
-    let yl = y1 - ((angle + _branch_angle).sin() * length);
+    let xr = x1 + ((angle - self._branch_angle).cos() * length);
+    let yr = y1 - ((angle - self._branch_angle).sin() * length);
+    let xl = x1 + ((angle + self._branch_angle).cos() * length);
+    let yl = y1 - ((angle + self._branch_angle).sin() * length);
     let p1 = pos2(x1, y1);
         let p2 = pos2(xr, yr);
         let p3 = pos2(xl, yl);
       //     print!("{},{}" ,p2.x,p2.y);
-        self.paint_line([p1, p2], Color32::from_rgb(255, 0, 0), 0.5, painter );
-        self.paint_line([p1, p3], Color32::from_rgb(255, 0, 0), 0.5, painter);
+        self.paint_line([p1, p2], Color32::from_rgb(self.r, self.g, self.b), 0.5, painter );
+        self.paint_line([p1, p3], Color32::from_rgb(self.r, self.g, self.b), 0.5, painter);
          if length > _min_branch_length{
                 self.drawtree(
                                     length * _scaling_factor,
                                     xr,
                                     yr,
-                                    angle - _branch_angle,
-                                    painter
+                                    angle - self._branch_angle,
+                                    painter,
+                                    start
                                 );
                                 
                                 self.drawtree(
                                     length * _scaling_factor,
                                     xl,
                                     yl,
-                                    angle + _branch_angle,
-                                    painter
+                                    angle +self. _branch_angle,
+                                    painter, 
+                                    start
                                 ); }
+         else{
+            let end = PreciseTime::now();
+            println!("{} seconds for single thread.", start.to(end));
+         }
 
        
     }
@@ -121,9 +166,11 @@ impl HTree {
         self.shapes= Vec::new();
 
         let rect = painter.clip_rect();
+        let start = PreciseTime::now();
+
 
         //length: f32, x1: f32, y1: f32, angle: f32, to_screen: &emath::RectTransform, shapes: &mut Vec<Shape>
-        self.drawtree(100.0, rect.width() / 2.0, rect.height() , 1.5708,painter);
+        self.drawtree(100.0, rect.width() / 2.0, rect.height() , 1.5708,painter, start);
 
        
         let mut x : std::vec::Vec<Shape> = Vec::new();
